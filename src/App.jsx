5ccc15fe25273a54e0c475760eae478821d0da73
harshpx <harsh.rzf@gmail.com>
2024-05-25 23:27:38 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+
+const App = () => {
+    const [htmlPage, setHtmlPage] = useState(null);
+
+    useEffect(() => {
+        fetchHtml();
+    },[]);
+
+    const fetchHtml = async () => {
+        try {
+            const response = await fetch('src/vanilla/flower.html');
+            if (!response.ok) {
+                throw new Error('Failed to fetch HTML');
+            }
+            const html = await response.text();
+            console.log(html);
+            setHtmlPage(html);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    return (
+        <div dangerouslySetInnerHTML={{__html:htmlPage}}></div>
+    )
+};
+
+export default App;
